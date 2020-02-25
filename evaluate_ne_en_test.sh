fbcd806e57a162d1ff402b9269f5f56bf2d4aefb
Jonne Saleva (Azure) <jonne@jonnesaleva.com>
2020-02-25 03:10:00 +0000

    add custom scripts to encapsulate training and evaluation of baseline

---
+++ evaluate_ne_en_test.sh
@@ -0,0 +1,10 @@
+CHECKPOINT_DIR="/checkpoints/flores/checkpoints_ne_en/checkpoint_best.pt"
+
+fairseq-generate \
+    data-bin/wiki_ne_en_bpe5000/ \
+    --source-lang ne --target-lang en \
+    --path $CHECKPOINT_DIR \
+    --beam 5 --lenpen 1.2 \
+    --gen-subset test \
+    --remove-bpe=sentencepiece \
+    --sacrebleu
