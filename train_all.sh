2b486bd60df99349517c0cacc88f69d964907c0f
Jonne Saleva <jonne@jonnesaleva.com>
2020-03-03 12:50:31 -0500

    start adding fairseq commad to train_all.sh

--- train_all.sh
+++ train_all.sh
@@ -7,6 +7,42 @@ About to train the following language pairs:
 - SI-EN
 - EN-SI
 """
+
+fairseq_train () {
+    SRC_LANG=$1
+    TGT_LANG=$2
+    CHECKPOINT_DIR=$3
+
+    # infer data directory
+    if [ "$SRC_LANG" = "si" ] || [ "$TGT_LANG" = "si" ];
+        DATA_FOLDER="data-bin/wiki_si_en_bpe5000/"
+    then
+    else
+        DATA_FOLDER="data-bin/wiki_ne_en_bpe5000/"
+    fi
+
+    CUDA_VISIBLE_DEVICES=0 fairseq-train \
+        $DATA_FOLDER \
+        --source-lang $SRC_LANG --target-lang $TGT_LANG \
+        --arch transformer --share-all-embeddings \
+        --encoder-layers 5 --decoder-layers 5 \
+        --encoder-embed-dim 512 --decoder-embed-dim 512 \
+        --encoder-ffn-embed-dim 2048 --decoder-ffn-embed-dim 2048 \
+        --encoder-attention-heads 2 --decoder-attention-heads 2 \
+        --encoder-normalize-before --decoder-normalize-before \
+        --dropout 0.4 --attention-dropout 0.2 --relu-dropout 0.2 \
+        --weight-decay 0.0001 \
+        --label-smoothing 0.2 --criterion label_smoothed_cross_entropy \
+        --optimizer adam --adam-betas '(0.9, 0.98)' --clip-norm 0 \
+        --lr-scheduler inverse_sqrt --warmup-updates 4000 --warmup-init-lr 1e-7 \
+        --lr 1e-3 --min-lr 1e-9 \
+        --max-tokens 4000 \
+        --update-freq 4 \
+        --max-epoch 100 \
+        --save-interval 10 \
+        --save-dir $CHECKPOINT_DIR
+}
+
 train () {
 
     SRC_LANG=$1
@@ -18,7 +54,6 @@ train () {
     TRAIN_SCRIPT_FOLDER="./train"
     LOG_FOLDER="./log"
     TIME_SUFFIX=$(date -Iminutes | sed s/':'/'-'/g)
-
 
     # create path to training file
     TRAIN_SCRIPT="train_baseline_"$SRC_LANG"_"$TGT_LANG".sh"
