08b9449128a724157af557d3af3c315d46ac4c72
j0ma <jonne@jonnesaleva.com>
2020-07-14 23:22:31 -0400

    add script to score with ChrF3 and write to stdout

---
+++ scripts/score-with-chrf.py
@@ -0,0 +1,43 @@
+from nltk.translate.chrf_score import corpus_chrf
+import click
+import sys
+
+
+def read_lines(fp):
+    with open(fp, "r") as f:
+        lines = [line.strip() for line in f.readlines()]
+
+        return lines
+
+
+@click.command()
+@click.option("--hypotheses-file")
+@click.option("--references-file")
+@click.option("--min-len", default=1)
+@click.option("--max-len", default=6)
+@click.option("--beta", default=3)
+@click.option("--include-whitespace", is_flag=True, default=False)
+def main(
+    hypotheses_file,
+    references_file,
+    min_len,
+    max_len,
+    beta,
+    include_whitespace,
+):
+    hypotheses = [h.split() for h in read_lines(hypotheses_file)]
+    references = [r.split() for r in read_lines(references_file)]
+    ignore_whitespace = not include_whitespace
+    score = corpus_chrf(
+        references=references,
+        hypotheses=hypotheses,
+        min_len=min_len,
+        max_len=max_len,
+        beta=beta,
+        ignore_whitespace=ignore_whitespace,
+    )
+    sys.stdout.write(f"ChrF{beta} = {score}\n")
+
+
+if __name__ == "__main__":
+    main()
