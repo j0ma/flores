2d0998a74c828118cdad4e09257669b754e8330b
jonne <jonne@jonnesaleva.com>
2020-11-10 16:25:04 -0500

    .

--- scripts/create-translation-output-folders.sh
+++ scripts/create-translation-output-folders.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+BPE_SIZES=(1000 2500 7500 10000)
 set -euo pipefail
 
 for model in "lmvr" "lmvr-tuned" "subword-nmt" "morsel" "baseline"
@@ -8,5 +9,8 @@ do
     do
         mkdir -p "$(pwd)/translation-output/${model}/seed-${seed}"
         mkdir -p "$(pwd)/translation-output-wmt19/${model}/seed-${seed}"
+        for bpe_size in "${BPE_SIZES[@]}"; do
+            mkdir -p "$(pwd)/translation-output-wmt19-bpe${bpe_size}/${model}/seed-${seed}"
+        done
     done
 done
