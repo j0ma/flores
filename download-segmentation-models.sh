9dc4ba37793ccc075768df2f96b3381269a020d6
Jonne Saleva <jonnesaleva@brandeis.edu>
2021-02-11 17:56:54 -0500

    225k zip download

--- download-segmentation-models.sh
+++ download-segmentation-models.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/sh
 
-ZIP_FILE=$1
-if [ -z "${ZIP_FILE}" ]; then
-    ZIP_FILE="segmentation-models.zip"
-fi
-URL_STUB="https://j0ma.keybase.pub/models/"
-
-wget "${URL_STUB}/${ZIP_FILE}"
-unzip "${ZIP_FILE}"
-rm "${ZIP_FILE}"
+for ZIP_FILE in "segmentation-models.zip" "segmentation-models-225k.zip"
+do
+    URL_STUB="https://j0ma.keybase.pub/models/"
+    wget "${URL_STUB}/${ZIP_FILE}"
+    unzip "${ZIP_FILE}"
+    rm "${ZIP_FILE}"
+done
